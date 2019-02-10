2f025f69f85e638b37080977ac7cae8b769b2820
88denooyer <88denooyer@cua.edu>
2019-02-09 23:22:05 -0500

    include data files

--- randomDataGen.js
+++ randomDataGen.js
@@ -12,13 +12,45 @@
 //var pain = 10;
 //var flex = 5;
 //var idNumber = 69;
+const FILE_SIZE = 601;
+var fs = require("fs");
+
 var object = {};
+
+function createPoorIndex(object){
+    object = {};
+    for(var i = 1; i < FILE_SIZE; i++){
+        object["Patient " + i] = [{"Reported Daily Pain": getRandomStat(8, 11),
+            "Reported Daily Flexibility": getRandomStat(1, 4)}];
+    }
+    return object;
+}
+
+function createAverageIndex(object){
+    object = {};
+    for(var i = 1; i < FILE_SIZE; i++){
+        object["Patient " + i] = [{"Reported Daily Pain": getRandomStat(4, 8),
+            "Reported Daily Flexibility": getRandomStat(4, 8)}];
+    }
+    return object;
+}
+
+function createGoodIndex(object){
+    object = {};
+    for(var i = 1; i < FILE_SIZE; i++){
+        object["Patient " + i] = [{"Reported Daily Pain": getRandomStat(1, 4),
+            "Reported Daily Flexibility": getRandomStat(8, 11)}];
+    }
+    return object;
+}
+
 
 function createIndex(object){
     object = {};
     for(var i = 1; i < 11; i++){
         // uses random int function to get pain and flex scores
-        object[i] = [{"pain": getRandomStat(0, 10), "flex": getRandomStat(0, 10)}];
+        object["Patient " + i] = [{"Reported Daily Pain": getRandomStat(0, 10),
+            "Reported Daily Flexibility": getRandomStat(0, 10)}];
         //object[i] = [{"pain": pain, "flex": flex}]; --> old test portion
     }
     return object;
@@ -31,10 +63,46 @@ function getRandomStat(lower, upper){
     return Math.floor(Math.random() * (upper - lower)) + lower;
 }
 
-x = createIndex(object);
+//x = createIndex(object);
+y = createGoodIndex(object);
+z = createPoorIndex(object);
+a = createAverageIndex(object);
 
-const prettyPrint = JSON.stringify(x, null, 2);
-console.log(prettyPrint);
+const prettyGood    = JSON.stringify(y, null, 2);
+const prettyPoor    = JSON.stringify(z, null, 2);
+const prettyAverage = JSON.stringify(a, null, 2);
+/*
+console.log("Poor Patient");
+console.log(prettyPoor);
+console.log("=============");
+console.log("Good Patient");
+console.log(prettyGood);
+*/
+
+
+fs.writeFile("./poor.json", prettyPoor, (err) => {
+    if(err){
+        console.error(err);
+        return;
+    };
+    console.log("Poor file was created");
+});
+
+fs.writeFile("./good.json", prettyGood, (err) => {
+    if(err){
+        console.error(err);
+        return;
+    };
+    console.log("Good file was created");
+});
+
+fs.writeFile("./average.json", prettyAverage, (err) => {
+    if(err){
+        console.error(err);
+        return;
+    };
+    console.log("Average file was created");
+});
 
 
 
